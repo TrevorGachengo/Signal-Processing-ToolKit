9b5edf9faa320f5bca0b88828e333e9efaa00e37
Trevor Gachengo <tgachengo@gmail.com>
2024-11-20 23:33:46 +0000

    Update signal.cpp

--- spt/signal.cpp
+++ spt/signal.cpp
@@ -1,4 +1,4 @@
-#include "signal.h"  // Include the header file
+#include "signal.h"
 
 Signal::Signal(float _amplitude, float _frequency, float _phase) {
     amplitude = _amplitude;
@@ -6,7 +6,6 @@ Signal::Signal(float _amplitude, float _frequency, float _phase) {
     phase = _phase;
 }
 
-// Method to calculate the signal value at time t
 float Signal::evaluate(float t) {
     return amplitude * sin(2.0 * M_PI * frequency * t + phase);
 }
