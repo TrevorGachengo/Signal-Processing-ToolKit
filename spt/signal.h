4c7dc7212ee285371c2f8d78aeab9cb54c03a5c1
Trevor Gachengo <tgachengo@gmail.com>
2024-11-20 23:19:06 +0000

    Update signal.h

--- spt/signal.h
+++ spt/signal.h
@@ -2,6 +2,10 @@
 #define STRUCT_H
 #include <cmath>
 
+/* 
+TODO:
+Generalize to accomodate for box signals, impulses, etc.
+*/
 struct Signal{
     // A * sin((2*pi*f)t + phase)
     float amplitude, frequency, phase;
